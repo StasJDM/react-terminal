84cbaf02ac25053d1ec6f3fe19b85a43ec2c10ca
StasJDM <pyatnicyn.stanislav@yandex.ru>
2021-08-04 23:55:53 +0300

    Implemented dynamic input for terminal

--- src/components/TerminalContent/TerminalContent.tsx
+++ src/components/TerminalContent/TerminalContent.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import TerminalInput from '../TerminalInput/TerminalInput';
 import './style.css';
 
-const TerminalContent = () => (
-  <div className='terminal_content'>
-    <TerminalInput />
-  </div>
-);
+const TerminalContent = () => {
+  const onPressedEnter = () => {
+    const oldTerminalStrings = terminalStrings;
+    if (oldTerminalStrings.length) {
+      oldTerminalStrings[oldTerminalStrings.length - 1].disabled = true;
+    }
+    setTerminalStrings([...oldTerminalStrings, { text: '', disabled: false }]);
+  };
+
+  const [terminalStrings, setTerminalStrings] = useState<{ text: string; disabled: boolean }[]>([
+    { text: '', disabled: false },
+  ]);
+
+  return (
+    <div className='terminal_content'>
+      {terminalStrings.map((terminalString, index) => (
+        <TerminalInput
+          onPressedEnter={onPressedEnter}
+          value={terminalString.text}
+          key={index}
+          disabled={terminalString.disabled}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default TerminalContent;
--- src/components/TerminalInput/TerminalInput.tsx
+++ src/components/TerminalInput/TerminalInput.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { KeyboardEvent, useState } from 'react';
 import './style.css';
 
-const TerminalInput = () => (
-  <div className='terminal-input-container'>
-    <span className='terminal-greeting'>stas:&gt;</span>
-    <input className='terminal-input' autoFocus />
-  </div>
-);
+type TerminalInputProps = {
+  value: string;
+  disabled: boolean;
+  onPressedEnter: () => void;
+};
+
+const TerminalInput = ({ value, disabled, onPressedEnter }: TerminalInputProps) => {
+  const [inputValue, setInputValue] = useState<string>(value);
+
+  const handleOnKeyDownEnter = (event: KeyboardEvent): void => {
+    if (event.code === 'Enter') {
+      onPressedEnter();
+    }
+  };
+
+  const handleOnInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className='terminal-input-container'>
+      <span className='terminal-greeting'>stas:&gt;</span>
+      {disabled || (
+        <input
+          className='terminal-input'
+          onKeyDown={handleOnKeyDownEnter}
+          onChange={handleOnInputChange}
+          value={inputValue}
+          autoFocus
+        />
+      )}
+      {!disabled || <span>{inputValue}</span>}
+    </div>
+  );
+};
 
 export default TerminalInput;
